3a0134eb9e6ac2c9c2ba1c74d8255d9e77cb880a
PowerKuu <hkleven6@outlook.com>
2023-03-14 21:33:08 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -3,32 +3,33 @@ import axios from "axios"
 
 
 const app = express()
-// Or environment variable
-const port = 3000
+
 const config = {
-    StatensVegvesenAuthorization: "7d099692-1dc9-43c2-a195-dc1882e0751a"
+    port: 3000,
+    statensVegvesenAuthorization: "7d099692-1dc9-43c2-a195-dc1882e0751a"
 }
-    
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post("/api/regnr", async (req, res) => {
-    const { regnr } = req.body
-    console.log(req.body)
+async function requestVegvesene(path: string) {
+    const url = `https://www.vegvesen.no${path}`
 
-    const url = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke=${regnr}`
     const response = await axios.get(url, { headers: {
-        "SVV-Authorization": `Apikey ${config.StatensVegvesenAuthorization}`
+        "SVV-Authorization": `Apikey ${config.statensVegvesenAuthorization}`
     }})
 
-    const data = response.data
+    return response.data
+}
 
-    console.log(data)
-    
+app.post("/api/regnr", async (req, res) => {
+    const { regnr } = req.body
+
+    const data = await requestVegvesene(`/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke=${regnr}`)
     
     res.json(data)
 })
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+app.listen(config.port, () => {
+    console.log(`Server started on port ${config.port}`)
 })
